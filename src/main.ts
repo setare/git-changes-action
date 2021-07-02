87d14fc1755cb5d98f79930076aa4e2e3ce11bcb
J. Santos <jamillo@gmail.com>
2021-07-01 21:55:04 -0300

    fix: Fix TS triple equal error

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,36 @@
 import * as core from '@actions/core'
-import { exec } from '@actions/exec'
+import {exec} from '@actions/exec'
 import github from '@actions/github'
 
-const INPUT_GITHUB_TOKEN = "github_token"
+const INPUT_GITHUB_TOKEN = 'github_token'
 
 async function run(): Promise<void> {
-  const token = core.getInput(INPUT_GITHUB_TOKEN);
-  
-  const octokit = github.getOctokit(token);
-  const ctx = github.context;
+  const token = core.getInput(INPUT_GITHUB_TOKEN)
 
+  const octokit = github.getOctokit(token)
+  const ctx = github.context
 
   const check = await octokit.rest.checks.create({
     ...ctx.repo,
     head_sha: ctx.sha,
     name: 'git-changes',
     status: 'in_progress'
-  });
-  
-  try {
-    let diffIndexOutput = '';
+  })
 
-    const exitCode = await exec('git', ['diff-index', '--quiet', 'HEAD', '--'], {
-      listeners: {
-        stdout: (data: Buffer) => {
-          diffIndexOutput += data.toString();
+  try {
+    let diffIndexOutput = ''
+
+    const exitCode = await exec(
+      'git',
+      ['diff-index', '--quiet', 'HEAD', '--'],
+      {
+        listeners: {
+          stdout: (data: Buffer) => {
+            diffIndexOutput += data.toString()
+          }
         }
       }
-    });
+    )
     if (exitCode == 0) {
       octokit.rest.checks.update({
         ...ctx.repo,
@@ -35,14 +38,20 @@ async function run(): Promise<void> {
         conclusion: 'success',
         output: {
           title: 'No changes were found',
-          summary: 'The repository has no uncommitted changes.',
-        },
+          summary: 'The repository has no uncommitted changes.'
+        }
       })
       return
     }
 
-    const files = diffIndexOutput.split('\n').map(s => s.trim().split(/ +/).filter((_, idx) => idx > 4).join(' '))
-    
+    const files = diffIndexOutput.split('\n').map(s =>
+      s
+        .trim()
+        .split(/ +/)
+        .filter((_, idx) => idx > 4)
+        .join(' ')
+    )
+
     octokit.rest.checks.update({
       ...ctx.repo,
       check_run_id: check.data.id,
@@ -54,7 +63,7 @@ async function run(): Promise<void> {
 ### Files
 ${files.map(f => `- ${f}`).join('\n')}
         `
-      },
+      }
     })
   } catch (e) {
     octokit.rest.checks.update({
@@ -67,11 +76,10 @@ ${files.map(f => `- ${f}`).join('\n')}
         text: `
 ## Exception
 ${e}
-`,
-      },
+`
+      }
     })
   }
-
 }
 
 run()
