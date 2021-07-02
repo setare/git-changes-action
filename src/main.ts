5bc6c10db0c93632752878ec6e368baafc9763ad
J. Santos <jamillo@gmail.com>
2021-07-01 21:30:34 -0300

    feat: Implement checks

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,77 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import { exec } from '@actions/exec'
+import github from '@actions/github'
+
+const INPUT_GITHUB_TOKEN = "github_token"
 
 async function run(): Promise<void> {
+  const token = core.getInput(INPUT_GITHUB_TOKEN);
+  
+  const octokit = github.getOctokit(token);
+  const ctx = github.context;
+
+
+  const check = await octokit.rest.checks.create({
+    ...ctx.repo,
+    head_sha: ctx.sha,
+    name: 'git-changes',
+    status: 'in_progress'
+  });
+  
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    let diffIndexOutput = '';
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const exitCode = await exec('git', ['diff-index', '--quiet', 'HEAD', '--'], {
+      listeners: {
+        stdout: (data: Buffer) => {
+          diffIndexOutput += data.toString();
+        }
+      }
+    });
+    if (exitCode == 0) {
+      octokit.rest.checks.update({
+        ...ctx.repo,
+        check_run_id: check.data.id,
+        conclusion: 'success',
+        output: {
+          title: 'No changes were found',
+          summary: 'The repository has no uncommitted changes.',
+        },
+      })
+      return
+    }
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
+    const files = diffIndexOutput.split('\n').map(s => s.trim().split(/ +/).filter((_, idx) => idx > 4).join(' '))
+    
+    octokit.rest.checks.update({
+      ...ctx.repo,
+      check_run_id: check.data.id,
+      conclusion: 'failure',
+      output: {
+        title: 'Uncommitted changes were found',
+        summary: `${files.length} uncommitted files were found`,
+        text: `
+### Files
+${files.map(f => `- ${f}`).join('\n')}
+        `
+      },
+    })
+  } catch (e) {
+    octokit.rest.checks.update({
+      ...ctx.repo,
+      check_run_id: check.data.id,
+      conclusion: 'failure',
+      output: {
+        title: 'Check failed',
+        summary: 'An error occurred when running the action.',
+        text: `
+## Exception
+${e}
+`,
+      },
+    })
   }
+
 }
 
 run()
