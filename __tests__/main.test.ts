c82d69ba565b7e928447433775a885bcb73a7abd
Jota Santos <jamillo@gmail.com>
2021-07-04 00:12:49 -0300

    fix: Improve error handling (#8)

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -2,26 +2,18 @@ import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 
-test('throws invalid number', async () => {
-  // gconst input = parseInt('foo', 10)
-  // await expect(wait(input)).rejects.toThrow('milliseconds not a number')
+test('test runs', () => {
+  // TODO
 })
-
-test('wait 500 ms', async () => {
-  // const start = new Date()
-  // await wait(500)
-  // const end = new Date()
-  // var delta = Math.abs(end.getTime() - start.getTime())
-  // expect(delta).toBeGreaterThan(450)
-})
-
+/*
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  // process.env['INPUT_MILLISECONDS'] = '500'
-  // const np = process.execPath
-  // const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  // const options: cp.ExecFileSyncOptions = {
-  //   env: process.env
-  // }
-  // console.log(cp.execFileSync(np, [ip], options).toString())
+  process.env['INPUT_GITHUB_TOKEN'] = '<github token>'
+  const np = process.execPath
+  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  const options: cp.ExecFileSyncOptions = {
+    env: process.env
+  }
+  console.log(cp.execFileSync(np, [ip], options).toString())
 })
+*/
